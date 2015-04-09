254731ab982a31cd34923ac02a539cde59ba2876
Thomas Steur <thomas.steur@gmail.com>
2015-04-09 00:05:41 +0000

    search only when clicking on search icon, not on every change

--- plugins/MultiSites/MultiSites.php
+++ plugins/MultiSites/MultiSites.php
@@ -68,6 +68,7 @@ class MultiSites extends \Piwik\Plugin
         $translations[] = 'MultiSites_LoadingWebsites';
         $translations[] = 'General_ErrorRequest';
         $translations[] = 'MultiSites_Pagination';
+        $translations[] = 'General_ClickToSearch';
     }
 
     public function getJsFiles(&$jsFiles)
